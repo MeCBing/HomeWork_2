9b767b4cc846f4b6fd9c698680ad71f61963506b
hi <hi@DESKTOP-T18BI0L>
2018-03-09 22:06:46 +0800

    單元測試

---
+++ ApplicationTests/AppLicationTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tests
+{
+    [TestClass()]
+    public class AppLicationTests
+    {
+        [TestMethod()]
+        public void ADDTest()
+        {
+            Number number = new AppLication();
+            number.ADD();
+            foreach(var index in number.list)
+            {
+            }
+        }
+    }
+}
