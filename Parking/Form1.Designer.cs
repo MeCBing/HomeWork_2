2936f1eb8744b5d60337e959e4fd5f32d9368a21
hi <hi@DESKTOP-T18BI0L>
2018-03-12 01:06:12 +0800

    Parking

---
+++ Parking/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Parking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 61);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(301, 25);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "進停車場時間";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "出停車場時間";
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(16, 155);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(297, 25);
+            this.dateTimePicker3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 49);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "結算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(91, 282);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "總金額 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Parking/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parking
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            SetDateTime();
+        }
+        public void SetDateTime()
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "yyyy/MM/dd tt hh:mm";
+            dateTimePicker1.ShowUpDown = true;
+            dateTimePicker3.Format = DateTimePickerFormat.Custom;
+            dateTimePicker3.CustomFormat = "yyyy/MM/dd tt hh:mm";
+            dateTimePicker3.ShowUpDown = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int money = 0;
+            DateTime time = dateTimePicker1.Value;
+            DateTime time1 = dateTimePicker3.Value;
+            TimeSpan span = time1 - time;
+            //MessageBox.Show($"{time.Hour}");
+            if(span.Minutes<0)
+            {
+                MessageBox.Show("請勿亂輸入");
+            }
+            else
+            {
+                int day = span.Days;
+                int hour = span.Hours;
+                int minute = span.Minutes;
+                hour = hour + (day * 24);
+                if (time.Minute == time1.Minute)
+                {
+                    hour++;
+                    minute = 0;
+                }
+                if (hour < 2)
+                {
+                    money = (hour * 2 * 30) + (minute / 30 * 30);
+                }
+                else if (hour < 4)
+                {
+                    money = ((hour - 2) * 2 * 40) + (minute / 30 * 40) + (2 * 2 * 30);
+                }
+                else
+                {
+                    money = ((hour - 4) * 2 * 60) + (minute / 30 * 60) + (2 * 2 * 30) + (2 * 2 * 40);
+                }
+                //MessageBox.Show($"{span}\n{money}");
+                label7.Text = " 總金額 :  " + money.ToString("C");
+            }
+        }
+    }
+}
